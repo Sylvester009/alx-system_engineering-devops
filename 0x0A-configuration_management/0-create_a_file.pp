ffa685862baf12ad1f0afab5468a47ea6ef03ea6
Samuel Sylvester <samsylvester09@gmail.com>
2024-04-19 21:53:11 +0100

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 #creates a file in /tmp directory
 
-file { "/tmp/school":
-    ensure => 'file',
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
-    content => 'I love Puppet',
+file { '/tmp/school':
+  ensure => 'file',
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+  content => 'I love Puppet',
 }
