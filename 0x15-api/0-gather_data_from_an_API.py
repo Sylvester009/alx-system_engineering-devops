7a43eedceddaeb602c4a1f86dc392cba7aba3f91
Samuel Sylvester <samsylvester09@gmail.com>
2024-05-21 02:00:56 +0100

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+Gather data from an API
+'''
+
+import re
+import requests
+import sys
+
+REST_API = "https://jsonplaceholder.typicode.com"
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            request = requests.get('{}/users/{}'.format(REST_API, id)).json()
+            task_request = requests.get('{}/todos'.format(REST_API)).json()
+            empty_name = request.get('name')
+            tasks = list(filter(lambda x: x.get('userId') == id, task_request))
+            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    empty_name,
+                    len(completed_tasks),
+                    len(tasks)
+                )
+            )
+            if len(completed_tasks) > 0:
+                for task in completed_tasks:
+                    print('\t {}'.format(task.get('title')))
