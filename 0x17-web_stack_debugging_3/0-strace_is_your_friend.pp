e1386af49b5ae49b7ebce268d9b825109d0cc44f
Samuel Sylvester <samsylvester09@gmail.com>
2024-06-05 17:15:49 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 # 0. Strace is your friend
 
-exec { 'fix-wordpress-error':
+exec { 'fix-apache':
         command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
         provider => 'shell'
-        path    => '/usr/local/bin/:/bin/'
 }
