cbfa83dcad68dd368efb683b8f44fbcdadc22c57
Samuel Sylvester <samsylvester09@gmail.com>
2024-06-05 15:48:15 +0100

    fix on check 5 and 6

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,8 +5,11 @@
 
 import requests
 
-
 def number_of_subscribers(subreddit):
+    # Check if the subreddit is valid
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
 
