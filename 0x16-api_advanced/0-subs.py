fb999ad02e4401b03eff6a73f8331d345b7b9260
Samuel Sylvester <samsylvester09@gmail.com>
2024-06-05 15:55:19 +0100

    Update 0-subs.py2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,7 +3,7 @@
 0. How many subs?
 """
 
-import requests
+from requests import get
 
 def number_of_subscribers(subreddit):
     # Check if the subreddit is valid
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
         return 0
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Mozilla/5.0"}
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
