27f9e7c2932249a87db79764a3a1b0f620678d8c
Samuel Sylvester <samsylvester09@gmail.com>
2024-06-05 15:58:24 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,6 +6,10 @@
 from requests import get
 
 def number_of_subscribers(subreddit):
+    """
+    Function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
     # Check if the subreddit is valid
     if subreddit is None or not isinstance(subreddit, str):
         return 0
@@ -13,7 +17,7 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = get(url, headers=user_agent, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
