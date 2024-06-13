5fd5aa7a1016abea4705959b383092c201b2058f
Samuel Sylvester <samsylvester09@gmail.com>
2024-06-13 17:12:10 +0100

    Create 1-user_limit.pp

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+## Change the OS configuration
+
+exec {'OS configuration':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
